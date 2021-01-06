5fb063dc34f6269c609396a7b4de4cefae8d00d1
Critiano Bertolini <bertolini10@gmail.com>
2021-01-05 21:30:12 -0300

    task 5 Repositories Created

---
+++ src/main/java/com/udacity/jdnd/course3/critter/repository/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.udacity.jdnd.course3.critter.repository;
+
+import com.udacity.jdnd.course3.critter.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+}
---
+++ src/main/java/com/udacity/jdnd/course3/critter/repository/PetRepository.java
@@ -0,0 +1,10 @@
+package com.udacity.jdnd.course3.critter.repository;
+
+import com.udacity.jdnd.course3.critter.model.Pet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PetRepository extends JpaRepository<Pet, Long> {
+
+}
